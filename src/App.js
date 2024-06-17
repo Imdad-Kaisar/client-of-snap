5c99c37029837f8e0f7fed033b7ea6206b8e0977
Imdad-Kaisar <ikaisar345@gamil.com>
2024-06-17 23:44:23 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import AddCategory from "./pages/AddCategory";
+import AddImage from "./pages/AddImage";
+import Header from "./pages/Header";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add-category" element={<AddCategory />} />
+        <Route path="/add-image" element={<AddImage />} />
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
